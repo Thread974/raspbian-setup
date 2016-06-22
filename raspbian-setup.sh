1cea0e87e8e7939689bfa6df842dd72393d62899
Frédéric Dalleau <fred@dalleau.re>
2016-06-22 11:08:17 +0200

    raspbian-setup: Add ssh key provisionning and rework command line arguments

--- raspbian-setup.sh
+++ raspbian-setup.sh
@@ -2,39 +2,68 @@
 
 set -e
 
-HOSTNAME=$1
-WIFI_NETWORK=$2
-WIFI_PSK=$3
+IMAGE=$1
+HOSTNAME=$2
+WIFI_NETWORK=$3
+WIFI_PSK=$4
+SSH_PUB_KEY=$5
+
+IMAGEFILE=$IMAGE
+
+offset_ext4()
+{
+	SECTOR=`sudo fdisk -l $IMAGEFILE | grep img2 | awk '{print $2}'`
+	echo "Got sector: $SECTOR"
+	OFFSET=`expr "$SECTOR" "*" "512"`
+}
 
 usage()
 {
-	echo "$0 hostname network psk"
+	echo "$0 image hostname network psk id_rsa.pub"
 }
 
 [ "$HOSTNAME" = "" ] && usage && exit 1
 [ "$WIFI_NETWORK" = "" ] && usage && exit 1
 [ "$WIFI_PSK" = "" ] && usage && exit 1
+[ "$IMAGE" = "" ] && usage && exit 1
+[ "$SSH_PUB_KEY" = "" ] && usage && exit 1
 
-mkdir -p ~/tmp
-cd ~/tmp
-cp /media/sf_fredo/Downloads/2016-05-27-raspbian-jessie-lite.img ./$HOSTNAME.img
+cp $IMAGEFILE ./$HOSTNAME.img
 
-ROOT=/home/fredo/tmp/root
+ROOT=./root
+
+offset_ext4
+echo "Calculated offset: $OFFSET"
 
 mkdir -p $ROOT
-mount -v -o offset=70254592 -t auto ./$HOSTNAME.img $ROOT
+mount -v -o offset=$OFFSET -t auto ./$HOSTNAME.img $ROOT
+
+# Hostname
 echo "$HOSTNAME" > $ROOT/etc/hostname
 cat $ROOT/etc/hosts | sed "s/raspberrypi/$HOSTNAME/" > $ROOT/etc/hosts2
 rm $ROOT/etc/hosts
 mv $ROOT/etc/hosts2 $ROOT/etc/hosts
 
+# Wifi settings
 WPA=$ROOT/etc/wpa_supplicant/wpa_supplicant.conf
 echo "network={" >> $WPA
 echo "    ssid=\"$WIFI_NETWORK\"" >> $WPA
 echo "    psk=\"$WIFI_PSK\"" >> $WPA
 echo "}" >> $WPA
 
-grep . $ROOT/etc/hostname $ROOT/etc/hosts $ROOT/etc/wpa_supplicant/wpa_supplicant.conf
+# SSH key
+mkdir -p $ROOT/home/pi/.ssh
+AUTH=$ROOT/home/pi/.ssh/authorized_keys
+cp $SSH_PUB_KEY $AUTH
+chmod 444 $AUTH
+# not working because pi user not exist on linux chown pi  $AUTH ; chgrp pi  $AUTH
 
+
+# Display result
+echo "Result:"
+grep . $ROOT/etc/hostname $ROOT/etc/hosts $ROOT/etc/wpa_supplicant/wpa_supplicant.conf $AUTH
+
+# Cleanup
 umount $ROOT
+rmdir $ROOT
 
